3326e94540bb719b64fe671270e62004bd6de16a
MathiasNeri <mathias.neri@ynov.com>
2023-10-12 23:12:22 +0200

    ajout fonction choisirMotAleatoire

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func choisirMotAleatoire(nomFichier string) (string, error) {
+	fichier, err := os.Open(nomFichier)
+	if err != nil {
+		return "", err
+	}
+	defer fichier.Close()
+	var mots []string
+	scanner := bufio.NewScanner(fichier)
+	for scanner.Scan() {
+		mots = append(mots, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	source := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(source)
+	indexAleatoire := rng.Intn(len(mots))
+	return mots[indexAleatoire], nil
+}
 
 func main() {
-	fmt.Println("zob")
+	nomFichier := "easy.txt"
+	mot, err := choisirMotAleatoire(nomFichier)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier :", err)
+		return
+	}
+	fmt.Printf("Le mot est : %s\n", mot)
 }
