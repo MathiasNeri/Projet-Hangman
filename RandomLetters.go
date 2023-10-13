4df9274565c42e66c6e8380f774bc52ca4e635b7
ademlsgo <145021201+ademlsgo@users.noreply.github.com>
2023-10-13 11:01:47 +0200

    Update RandomLetters.go

--- RandomLetters.go
+++ RandomLetters.go
@@ -1,32 +1,35 @@
 package projethangman
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
 )
 
 func RandomLetters() {
 	// La chaîne de caractères à partir de laquelle vous souhaitez extraire les lettres aléatoires
 	maChaine := "votre_chaine_de_caracteres"
 
-	// Initialiser le générateur de nombres aléatoires avec une graine (seed) basée sur le temps actuel
-	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 2; i++ {
+		// Générer deux indices aléatoires pour extraire deux lettres aléatoires de la chaîne
+		indice1, _ := rand.Int(rand.Reader, big.NewInt(int64(len(maChaine)))
+		indice2, _ := rand.Int(rand.Reader, big.NewInt(int64(len(maChaine)))
 
-	// Générer deux indices aléatoires pour extraire deux lettres aléatoires de la chaîne
-	indice1 := rand.Intn(len(maChaine))
-	indice2 := rand.Intn(len(maChaine))
+		// Obtenir les indices en utilisant Int64
+		i1 := indice1.Int64()
+		i2 := indice2.Int64()
 
-	// Créer une nouvelle chaîne de caractères avec des tirets pour cacher les autres caractères
-	chaineCachee := ""
-	for i := 0; i < len(maChaine); i++ {
-		if i == indice1 || i == indice2 {
-			chaineCachee += string(maChaine[i])
-		} else {
-			chaineCachee += "---"
+		// Créer une nouvelle chaîne de caractères avec des tirets pour cacher les autres caractères
+		chaineCachee := ""
+		for j := 0; j < len(maChaine); j++ {
+			if j == int(i1) || j == int(i2) {
+				chaineCachee += string(maChaine[j])
+			} else {
+				chaineCachee += "---"
+			}
 		}
-	}
 
-	// Afficher la chaîne cachée avec les deux lettres aléatoires
-	fmt.Println(chaineCachee)
+		// Afficher la chaîne cachée avec les lettres aléatoires
+		fmt.Println(chaineCachee)
+	}
 }
